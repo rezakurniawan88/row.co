93bd2716de52eef556496f9a05615d2ec6cd2233
Reza Kurniawan <67334603+rezakurniawan88@users.noreply.github.com>
2023-11-29 23:55:04 +0700

    feat: create homepage (#1)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        graySecondary: '#F0F0F0',
+        grayCard: '#F0EEED'
+      },
+      animation: {
+        marquee: 'marquee 15s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
